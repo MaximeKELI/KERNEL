5cb9c8e79f43a5c854d79069937ca98cdfa8568d
MaximeKELI <presidentnetero01@gmail.com>
2026-02-26 14:59:40 +0000

    All coders

---
+++ include/slab.h
@@ -0,0 +1,37 @@
+#ifndef SLAB_H
+#define SLAB_H
+
+#include "types.h"
+
+/* SLAB cache */
+typedef struct slab_cache {
+    char name[32];
+    size_t object_size;
+    size_t objects_per_slab;
+    void* slabs;
+    u32 active_objects;
+    u32 total_objects;
+    spinlock_t lock;
+    struct slab_cache* next;
+} slab_cache_t;
+
+/* Initialize SLAB allocator */
+void slab_init(void);
+
+/* Create SLAB cache */
+slab_cache_t* kmem_cache_create(const char* name, size_t size);
+
+/* Allocate from cache */
+void* kmem_cache_alloc(slab_cache_t* cache);
+
+/* Free to cache */
+void kmem_cache_free(slab_cache_t* cache, void* obj);
+
+/* Destroy cache */
+void kmem_cache_destroy(slab_cache_t* cache);
+
+/* Per-CPU cache operations */
+void* kmem_cache_alloc_cpu(slab_cache_t* cache, u32 cpu_id);
+void kmem_cache_free_cpu(slab_cache_t* cache, void* obj, u32 cpu_id);
+
+#endif /* SLAB_H */

c589df5f5e3a0498e509b0c56fbc2c73c0aa1874
MaximeKELI <presidentnetero01@gmail.com>
2026-02-26 14:46:23 +0000

    All coders

--- lib/stdio.c
+++ lib/stdio.c
@@ -214,3 +214,120 @@ void debug_puts(const char* str) {
         debug_putchar(*str++);
     }
 }
+
+int snprintf(char* str, size_t size, const char* format, ...) {
+    va_list args;
+    va_start(args, format);
+    int ret = vsnprintf(str, size, format, args);
+    va_end(args);
+    return ret;
+}
+
+int vsnprintf(char* str, size_t size, const char* format, va_list args) {
+    if (!str || size == 0) return 0;
+    
+    char* dest = str;
+    size_t remaining = size - 1;
+    const char* p = format;
+    int written = 0;
+    
+    while (*p && remaining > 0) {
+        if (*p == '%') {
+            p++;
+            switch (*p) {
+                case 'd':
+                case 'i': {
+                    int val = va_arg(args, int);
+                    char num[32];
+                    int i = 0;
+                    if (val < 0) {
+                        if (remaining > 0) {
+                            *dest++ = '-';
+                            remaining--;
+                            written++;
+                        }
+                        val = -val;
+                    }
+                    if (val == 0) {
+                        if (remaining > 0) {
+                            *dest++ = '0';
+                            remaining--;
+                            written++;
+                        }
+                    } else {
+                        while (val > 0 && i < 31) {
+                            num[i++] = '0' + (val % 10);
+                            val /= 10;
+                        }
+                        while (i > 0 && remaining > 0) {
+                            *dest++ = num[--i];
+                            remaining--;
+                            written++;
+                        }
+                    }
+                    break;
+                }
+                case 's': {
+                    const char* s = va_arg(args, const char*);
+                    if (!s) s = "(null)";
+                    while (*s && remaining > 0) {
+                        *dest++ = *s++;
+                        remaining--;
+                        written++;
+                    }
+                    break;
+                }
+                case 'c': {
+                    char c = (char)va_arg(args, int);
+                    if (remaining > 0) {
+                        *dest++ = c;
+                        remaining--;
+                        written++;
+                    }
+                    break;
+                }
+                case 'x': {
+                    u32 val = va_arg(args, u32);
+                    char hex[] = "0123456789abcdef";
+                    if (remaining >= 2) {
+                        *dest++ = '0';
+                        *dest++ = 'x';
+                        remaining -= 2;
+                        written += 2;
+                    }
+                    bool started = false;
+                    for (int i = 28; i >= 0; i -= 4) {
+                        u8 nibble = (val >> i) & 0xF;
+                        if (nibble || started || i == 0) {
+                            started = true;
+                            if (remaining > 0) {
+                                *dest++ = hex[nibble];
+                                remaining--;
+                                written++;
+                            }
+                        }
+                    }
+                    break;
+                }
+                default:
+                    if (remaining > 0) {
+                        *dest++ = '%';
+                        *dest++ = *p;
+                        remaining -= 2;
+                        written += 2;
+                    }
+                    break;
+            }
+        } else {
+            if (remaining > 0) {
+                *dest++ = *p;
+                remaining--;
+                written++;
+            }
+        }
+        p++;
+    }
+    
+    *dest = '\0';
+    return written;
+}

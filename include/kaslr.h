e24e91db4685bb3033db52095c7ffeecc30918f1
MaximeKELI <presidentnetero01@gmail.com>
2026-02-26 15:01:59 +0000

    All coders

---
+++ include/kaslr.h
@@ -0,0 +1,21 @@
+#ifndef KASLR_H
+#define KASLR_H
+
+#include "types.h"
+
+/* Initialize KASLR */
+void kaslr_init(void);
+
+/* Get random address offset */
+u64 kaslr_get_offset(void);
+
+/* Randomize kernel base */
+void kaslr_randomize_kernel_base(void);
+
+/* Randomize module base */
+u64 kaslr_randomize_module_base(void);
+
+/* Get ASLR status */
+bool kaslr_enabled(void);
+
+#endif /* KASLR_H */

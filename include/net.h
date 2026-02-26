cc17ee00eecc2d2e39c58c7848e641c9a0c702cc
MaximeKELI <presidentnetero01@gmail.com>
2026-02-26 14:57:59 +0000

    All coders

---
+++ include/net.h
@@ -0,0 +1,81 @@
+#ifndef NET_H
+#define NET_H
+
+#include "types.h"
+
+/* Network protocols */
+#define ETH_P_IP  0x0800
+#define ETH_P_ARP 0x0806
+
+/* IP protocol */
+#define IPPROTO_ICMP 1
+#define IPPROTO_TCP  6
+#define IPPROTO_UDP  17
+
+/* Socket types */
+#define SOCK_STREAM 1
+#define SOCK_DGRAM  2
+#define SOCK_RAW    3
+
+/* Socket address */
+typedef struct sockaddr {
+    u16 sa_family;
+    char sa_data[14];
+} sockaddr_t;
+
+/* IP address */
+typedef struct {
+    u8 addr[4];
+} ip_addr_t;
+
+/* Socket structure */
+typedef struct socket {
+    u32 domain;
+    u32 type;
+    u32 protocol;
+    ip_addr_t local_addr;
+    u16 local_port;
+    ip_addr_t remote_addr;
+    u16 remote_port;
+    void* private_data;
+    struct socket_ops* ops;
+} socket_t;
+
+/* Socket operations */
+typedef struct socket_ops {
+    int (*bind)(socket_t* sock, const sockaddr_t* addr);
+    int (*listen)(socket_t* sock, int backlog);
+    int (*accept)(socket_t* sock, sockaddr_t* addr);
+    int (*connect)(socket_t* sock, const sockaddr_t* addr);
+    ssize_t (*send)(socket_t* sock, const void* buf, size_t len);
+    ssize_t (*recv)(socket_t* sock, void* buf, size_t len);
+    int (*close)(socket_t* sock);
+} socket_ops_t;
+
+/* Network interface */
+typedef struct netif {
+    char name[16];
+    ip_addr_t ip;
+    ip_addr_t netmask;
+    ip_addr_t gateway;
+    u8 mac[6];
+    bool up;
+    struct netif* next;
+} netif_t;
+
+/* Initialize networking */
+void net_init(void);
+
+/* Create socket */
+socket_t* socket_create(int domain, int type, int protocol);
+
+/* Register network interface */
+int netif_register(netif_t* iface);
+
+/* Send packet */
+int net_send_packet(netif_t* iface, const void* data, size_t len);
+
+/* Receive packet */
+int net_recv_packet(netif_t* iface, void* data, size_t* len);
+
+#endif /* NET_H */

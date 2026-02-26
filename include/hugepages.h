2ab5d4b4278cec1ed4ad2c39af212a94e7a8b486
MaximeKELI <presidentnetero01@gmail.com>
2026-02-26 15:16:07 +0000

    All coders

---
+++ include/hugepages.h
@@ -0,0 +1,22 @@
+#ifndef HUGEPAGES_H
+#define HUGEPAGES_H
+
+#include "types.h"
+
+/* Huge page sizes */
+#define HUGEPAGE_2MB (2 * 1024 * 1024)
+#define HUGEPAGE_1GB (1024 * 1024 * 1024)
+
+/* Initialize huge pages */
+void hugepages_init(void);
+
+/* Allocate huge page */
+void* hugepage_alloc(size_t size);
+
+/* Free huge page */
+void hugepage_free(void* addr, size_t size);
+
+/* Get huge page statistics */
+void hugepages_get_stats(u64* total, u64* free, u64* used);
+
+#endif /* HUGEPAGES_H */

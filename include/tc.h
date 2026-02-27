297e97a3a914bf9a847eb0373f42b8bc09cf5c4e
MaximeKELI <presidentnetero01@gmail.com>
2026-02-27 00:40:32 +0000

    All coders

--- include/tc.h
+++ include/tc.h
@@ -2,12 +2,19 @@
 #define TC_H
 
 #include "types.h"
-
-/* Traffic Control */
+#include "skbuff.h"
 
 /* Initialize Traffic Control */
 void tc_init(void);
 
-/* TODO: Add function declarations */
+/* Add/delete qdisc */
+int tc_add_qdisc(const char* name, u32 handle, u32 parent);
+int tc_del_qdisc(u32 handle);
+
+/* Add class to qdisc */
+int tc_add_class(u32 qdisc_handle, u32 classid, u64 rate, u64 ceil, u64 burst);
+
+/* Enqueue packet (rate limiting) */
+int tc_enqueue(sk_buff_t* skb, u32 qdisc_handle, u32 classid);
 
 #endif /* TC_H */

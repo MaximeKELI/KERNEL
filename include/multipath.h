c887fe13ecd706fc0fcf5ac9f70f7e3f3b8cd09b
MaximeKELI <presidentnetero01@gmail.com>
2026-02-27 00:43:24 +0000

    All coders

--- include/multipath.h
+++ include/multipath.h
@@ -2,12 +2,20 @@
 #define MULTIPATH_H
 
 #include "types.h"
+#include "net.h"
+#include "route.h"
 
-/* Network Multipath */
-
-/* Initialize Network Multipath */
+/* Initialize multipath routing */
 void multipath_init(void);
 
-/* TODO: Add function declarations */
+/* Add multipath route */
+int multipath_add_route(ip_addr_t dst, ip_addr_t netmask, 
+                        route_entry_t** paths, u32 num_paths);
+
+/* Select path for destination */
+route_entry_t* multipath_select_path(ip_addr_t dst);
+
+/* Update path statistics */
+void multipath_update_stats(ip_addr_t dst, u32 path_index, u64 bytes);
 
 #endif /* MULTIPATH_H */

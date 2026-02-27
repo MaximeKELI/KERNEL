f1f931e4fc1c772141dcc141d9ad99a10adab33c
MaximeKELI <presidentnetero01@gmail.com>
2026-02-27 15:10:25 +0000

    All coders

--- include/media.h
+++ include/media.h
@@ -2,8 +2,21 @@
 #define MEDIA_H
 
 #include "types.h"
+#include "codec.h"
 
 /* Initialize media subsystem */
 void media_init(void);
+
+/* Detect media format from file extension */
+codec_format_t media_detect_format_from_extension(const char* filename);
+
+/* Detect media format from file header (magic bytes) */
+codec_format_t media_detect_format_from_header(const void* data, size_t len);
+
+/* Get codec type from format */
+codec_type_t media_get_codec_type(codec_format_t format);
+
+/* Get format name as string */
+const char* media_format_to_string(codec_format_t format);
 
 #endif /* MEDIA_H */

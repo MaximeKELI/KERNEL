10aac64b58a61d509bfaf07afe78c032b45ae761
MaximeKELI <presidentnetero01@gmail.com>
2026-02-27 00:38:48 +0000

    All coders

--- include/net_stats.h
+++ include/net_stats.h
@@ -3,11 +3,31 @@
 
 #include "types.h"
 
-/* Network Statistics */
+/* Network statistics */
+typedef struct {
+    u64 rx_packets;
+    u64 tx_packets;
+    u64 rx_bytes;
+    u64 tx_bytes;
+    u64 rx_errors;
+    u64 tx_errors;
+    u64 rx_dropped;
+    u64 tx_dropped;
+    u64 collisions;
+    u64 multicast;
+} net_stats_t;
 
-/* Initialize Network Statistics */
+/* Initialize network statistics */
 void net_stats_init(void);
 
-/* TODO: Add function declarations */
+/* Update statistics */
+void net_stats_update_rx(u64 bytes);
+void net_stats_update_tx(u64 bytes);
+void net_stats_update_rx_error(void);
+void net_stats_update_tx_error(void);
+
+/* Get/reset statistics */
+void net_stats_get(net_stats_t* stats);
+void net_stats_reset(void);
 
 #endif /* NET_STATS_H */

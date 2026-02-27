4375f7f168e87052a8e9a78e5bbe7bcda777f5d2
MaximeKELI <presidentnetero01@gmail.com>
2026-02-27 00:23:34 +0000

    All coders

--- include/evdev.h
+++ include/evdev.h
@@ -3,11 +3,27 @@
 
 #include "types.h"
 
-/* Input Devices */
+/* Input Event */
+typedef struct input_event input_event_t;
 
-/* Initialize Input Devices */
+/* Evdev Device */
+typedef struct evdev_device evdev_device_t;
+
+/* Initialize Evdev */
 void evdev_init(void);
 
-/* TODO: Add function declarations */
+/* Allocate/free evdev device */
+evdev_device_t* evdev_alloc_device(void);
+void evdev_free_device(evdev_device_t* dev);
+
+/* Write/read input events */
+int evdev_write_event(evdev_device_t* dev, input_event_t* event);
+int evdev_read_event(evdev_device_t* dev, input_event_t* event);
+
+/* Find device by name */
+evdev_device_t* evdev_find_device(const char* name);
+
+/* Get device count */
+u32 evdev_get_device_count(void);
 
 #endif /* EVDEV_H */

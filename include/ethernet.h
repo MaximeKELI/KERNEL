ad159dec9e95475ffa6e80d38f7ef6d486983c2d
MaximeKELI <presidentnetero01@gmail.com>
2026-02-27 00:19:43 +0000

    All coders

--- include/ethernet.h
+++ include/ethernet.h
@@ -3,11 +3,28 @@
 
 #include "types.h"
 
-/* Ethernet Drivers */
+/* Ethernet device */
+typedef struct ethernet_device ethernet_device_t;
 
-/* Initialize Ethernet Drivers */
+/* Initialize Ethernet drivers */
 void ethernet_init(void);
 
-/* TODO: Add function declarations */
+/* Allocate/free ethernet device */
+ethernet_device_t* ethernet_alloc_device(void);
+void ethernet_free_device(ethernet_device_t* dev);
+
+/* Bring device up/down */
+int ethernet_up(ethernet_device_t* dev);
+int ethernet_down(ethernet_device_t* dev);
+
+/* Send/receive packets */
+int ethernet_send_packet(ethernet_device_t* dev, void* data, size_t len);
+int ethernet_receive_packet(ethernet_device_t* dev, void* buffer, size_t buffer_size);
+
+/* Find device by name */
+ethernet_device_t* ethernet_find_device(const char* name);
+
+/* Get device count */
+u32 ethernet_get_device_count(void);
 
 #endif /* ETHERNET_H */

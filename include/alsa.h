8d43f09903e19dbf91749bb67bb089df0441ed44
MaximeKELI <presidentnetero01@gmail.com>
2026-02-27 00:22:10 +0000

    All coders

--- include/alsa.h
+++ include/alsa.h
@@ -3,11 +3,27 @@
 
 #include "types.h"
 
-/* ALSA Audio */
+/* ALSA Device */
+typedef struct alsa_device alsa_device_t;
 
-/* Initialize ALSA Audio */
+/* Initialize ALSA */
 void alsa_init(void);
 
-/* TODO: Add function declarations */
+/* Allocate/free ALSA device */
+alsa_device_t* alsa_alloc_device(void);
+void alsa_free_device(alsa_device_t* dev);
+
+/* Set audio parameters */
+int alsa_set_params(alsa_device_t* dev, u32 sample_rate, u32 channels, u32 bits_per_sample);
+
+/* Read/write audio data */
+int alsa_write(alsa_device_t* dev, void* data, size_t len);
+int alsa_read(alsa_device_t* dev, void* buffer, size_t len);
+
+/* Find device by name */
+alsa_device_t* alsa_find_device(const char* name);
+
+/* Get device count */
+u32 alsa_get_device_count(void);
 
 #endif /* ALSA_H */

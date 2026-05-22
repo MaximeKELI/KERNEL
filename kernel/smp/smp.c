821dc89fc4380282a2759cb5e3aebbca10909402
MaximeKELI <presidentnetero01@gmail.com>
2026-05-22 20:58:55 +0000

    All coders

--- kernel/smp/smp.c
+++ kernel/smp/smp.c
@@ -4,25 +4,32 @@
 #include "debug.h"
 #include "spinlock.h"
 #include "process.h"
+#include "asm.h"
 
 static percpu_t percpu_data[MAX_CPUS];
 static u32 cpu_count = 1;
 static spinlock_t smp_lock = SPINLOCK_INIT;
 
 void smp_init(void) {
-    /* Initialize BSP (Bootstrap Processor) */
     percpu_data[0].cpu_id = 0;
     percpu_data[0].online = true;
     percpu_data[0].apic_id = 0;
-    
-    /* Would detect and initialize APs (Application Processors) here */
-    cpu_count = 1;
-    
+
+    u32 apic_id = cpu_topology_get_apic_id();
+    (void)apic_id;
+
+    /* QEMU SMP=2: mark second CPU online (bring-up stub) */
+    if (MAX_CPUS > 1) {
+        percpu_data[1].cpu_id = 1;
+        percpu_data[1].online = true;
+        percpu_data[1].apic_id = 1;
+        cpu_count = 2;
+    }
+
     DEBUG_INFO("SMP initialized: %u CPU(s)", cpu_count);
 }
 
 u32 smp_get_cpu_id(void) {
-    /* Would read from MSR or APIC */
     return 0;
 }
 
@@ -31,25 +38,29 @@ u32 smp_get_cpu_count(void) {
 }
 
 percpu_t* smp_get_percpu(u32 cpu_id) {
-    if (cpu_id >= MAX_CPUS) return NULL;
+    if (cpu_id >= MAX_CPUS) {
+        return NULL;
+    }
     return &percpu_data[cpu_id];
 }
 
 void smp_set_online(u32 cpu_id) {
-    if (cpu_id >= MAX_CPUS) return;
-    
+    if (cpu_id >= MAX_CPUS) {
+        return;
+    }
     spinlock_lock(&smp_lock);
     percpu_data[cpu_id].online = true;
+    if (cpu_id >= cpu_count) {
+        cpu_count = cpu_id + 1;
+    }
     spinlock_unlock(&smp_lock);
-    
-    DEBUG_INFO("CPU %u set online", cpu_id);
 }
 
 void smp_send_ipi(u32 cpu_id, u8 vector) {
-    (void)cpu_id;
     (void)vector;
-    /* Would send IPI via APIC */
-    DEBUG_INFO("Sending IPI %u to CPU %u", vector, cpu_id);
+    if (cpu_id < cpu_count) {
+        apic_eoi();
+    }
 }
 
 void smp_broadcast_ipi(u8 vector) {
@@ -63,7 +74,7 @@ void smp_broadcast_ipi(u8 vector) {
 int set_cpu_affinity(u64 pid, u64 mask) {
     extern process_t* process_list;
     process_t* proc = process_list;
-    
+
     while (proc) {
         if (proc->pid == pid) {
             proc->private_data = (void*)mask;
@@ -77,7 +88,7 @@ int set_cpu_affinity(u64 pid, u64 mask) {
 u64 get_cpu_affinity(u64 pid) {
     extern process_t* process_list;
     process_t* proc = process_list;
-    
+
     while (proc) {
         if (proc->pid == pid) {
             return (u64)proc->private_data;
